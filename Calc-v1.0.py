b2e4915d3f9975f65176dab87f8967769b51c498
Bit77 <daren@markandtraci.com>
2025-10-28 14:10:50 -0500

    Update project date and goal in comments

--- Calc-v1.0.py
+++ Calc-v1.0.py
@@ -1,7 +1,7 @@
 #######################################
 ## The3DP                            ##
 ## Expected Finish Date:             ##
-## 10/28/2025                        ##
-## Goal: Make a corrected            ##
-## calculator using bare python      ##
+## 11/1/2025                         ##
+## Goal: Make an accurate            ##
+## calculator using bare Python.     ##
 #######################################
