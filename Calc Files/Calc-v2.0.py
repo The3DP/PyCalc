aa40ee84338f71917649cd1f4d0257a1d7d5da0b
Bit77 <daren@markandtraci.com>
2025-10-30 17:02:16 -0500

    Update Calc-v2.0.py

--- Calc Files/Calc-v2.0.py
+++ Calc Files/Calc-v2.0.py
@@ -9,33 +9,33 @@
 #######################################
 
 ###=================================== PSEUDOCODE =========================================##
-##BEGIN SMART_CALCULATOR                                                                   ##                                                                 
+##                                                                                         ##                                                                 
 ##                                                                                         ##
-##    IMPORT math module                                                                   ##
 ##                                                                                         ##
-##    DEFINE allowed_names AS dictionary of math functions and constants                   ##
-##        (exclude anything that starts with "__")                                         ##
 ##                                                                                         ##
-##    PRINT welcome messages and usage examples                                            ##
+##                      ##
+##                                                ##
 ##                                                                                         ##
-##    LOOP forever:                                                                        ##
-##        PROMPT user for an expression                                                    ##
+##                                              ##
 ##                                                                                         ##
-##        IF expression is "exit" OR "quit":                                               ##
-##            PRINT goodbye message                                                        ##
-##            BREAK the loop                                                               ##
+##                                                                          ##
+##                                                         ##
 ##                                                                                         ##
-##        TRY:                                                                             ##
-##            IF expression ends with "d)":                                                ##
-##                REPLACE "d)" with "*(pi/180))" to convert degrees to radians             ##
+##                                                    ##
+##                                                                ##
+##                                                                         ##
 ##                                                                                         ##
-##            EVALUATE expression using only allowed_names (safe eval)                     ##
-##            PRINT the result                                                             ##
+##                                                                                    ##
+##                                                          ##
+##                         ##
 ##                                                                                         ##
-##        EXCEPT if an error occurs:                                                       ##
-##            PRINT an error message with the exception details                            ##
+##                                 ##
+##                                                                         ##
 ##                                                                                         ##
-##    END LOOP                                                                             ##
+##                                                               ##
+##                                        ##
 ##                                                                                         ##
-##END PROGRAM                                                                              ##
+##                                                                                ##
+##                                                                                         ##
+##                                                                              ##
 ###========================================================================================##
